bcbd2127cbe7074baa15aa3710d07b5bcc370f8d
nhyeonu <uccccu10@gmail.com>
2022-12-10 07:37:20 +0900

    Add an empty HTTP response at /

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn root() -> impl Responder {
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(root)
+    }).bind(("127.0.0.1", 8080))?.run().await
 }
