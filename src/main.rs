1f3218509b547b35a3fef352cc565e1e50f517a5
nhyeonu <uccccu10@gmail.com>
2022-12-10 08:16:40 +0900

    Add a login page

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn root() -> impl Responder {
     HttpResponse::Ok()
+}
+
+#[get("/auth")]
+async fn auth_page() -> impl Responder {
+    let body = r#"
+<form action="/method="post">
+    <label for="username">Username: </label>
+    <input type="text" id="username" name="username" placeholder="Username">
+    <label for="password">Password: </label>
+    <input type="password" id="password" name="password" placeholder="Password">
+    <input type="submit" value="Login">
+</form>
+"#;
+    HttpResponse::Ok()
+        .body(body)
 }
 
 #[actix_web::main]
@@ -10,5 +25,6 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(root)
+            .service(auth_page)
     }).bind(("127.0.0.1", 8080))?.run().await
 }
