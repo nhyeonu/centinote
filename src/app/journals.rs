c406e886c0ab292ca170fa3239f4eb5b4217a9ba
nhyeonu <uccccu10@gmail.com>
2022-12-22 09:59:14 +0900

    Revert "refactor: Move the /api/journals/{entry_uuid} GET handler to it's own module"
    This reverts commit afed3fa8e353ec6423f2b02548d581421c19f708.

--- src/app/journals.rs
+++ src/app/journals.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
-use actix_web::{web, get, post, Responder, HttpRequest, HttpResponse};
+use actix_web::{get, web, post, Responder, HttpRequest, HttpResponse};
+use sqlx::Row;
 use uuid::Uuid;
 use crate::State;
 use crate::auth::utils;
@@ -36,66 +37,58 @@ async fn post(data: web::Data<State<'_>>, req: HttpRequest, info: web::Json<Jour
     }
 }
 
-pub mod entries {
-    use actix_web::{get, web, Responder, HttpRequest, HttpResponse};
-    use sqlx::Row;
-    use crate::State;
-    use crate::auth::utils;
-    use crate::app::journals::Journal;
+#[get("/journals/{entry_uuid}")]
+async fn get(data: web::Data<State<'_>>, req: HttpRequest, path: web::Path<String>) -> impl Responder {
+    let journal_entry_uuid = path.into_inner();
+    let journal_entry_query_result = sqlx::query("SELECT UserUuid AS user_uuid, Title AS title, Body AS body FROM journals WHERE Uuid = $1")
+        .bind(journal_entry_uuid)
+        .fetch_one(&data.db_pool)
+        .await;
 
-    #[get("/journals/{entry_uuid}")]
-    async fn get(data: web::Data<State<'_>>, req: HttpRequest, path: web::Path<String>) -> impl Responder {
-        let journal_entry_uuid = path.into_inner();
-        let journal_entry_query_result = sqlx::query("SELECT UserUuid AS user_uuid, Title AS title, Body AS body FROM journals WHERE Uuid = $1")
-            .bind(journal_entry_uuid)
-            .fetch_one(&data.db_pool)
-            .await;
-
-        let journal_entry_row = match journal_entry_query_result {
-            Ok(row) => row,
-            Err(error) => {
-                match error {
-                    sqlx::Error::RowNotFound => return HttpResponse::NotFound().finish(),
-                    _ => {
-                        println!("{}", error);
-                        return HttpResponse::InternalServerError().finish();
-                    }
+    let journal_entry_row = match journal_entry_query_result {
+        Ok(row) => row,
+        Err(error) => {
+            match error {
+                sqlx::Error::RowNotFound => return HttpResponse::NotFound().finish(),
+                _ => {
+                    println!("{}", error);
+                    return HttpResponse::InternalServerError().finish();
                 }
             }
-        };
-
-        let trusted_user_uuid = utils::verify_request!(&data.db_pool, &req);
-        let journal_user_uuid: String = match journal_entry_row.try_get("user_uuid") {
-            Ok(uuid) => uuid,
-            Err(error) => {
-                println!("{}", error);
-                return HttpResponse::InternalServerError().finish();
-            }
-        };
-
-        if trusted_user_uuid != journal_user_uuid {
-            return HttpResponse::Unauthorized().finish();
         }
+    };
 
-        let journal_title: String = match journal_entry_row.try_get("title") {
-            Ok(title) => title,
-            Err(error) => {
-                println!("{}", error);
-                return HttpResponse::InternalServerError().finish();
-            }
-        };
+    let trusted_user_uuid = utils::verify_request!(&data.db_pool, &req);
+    let journal_user_uuid: String = match journal_entry_row.try_get("user_uuid") {
+        Ok(uuid) => uuid,
+        Err(error) => {
+            println!("{}", error);
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
 
-        let journal_body: String = match journal_entry_row.try_get("title") {
-            Ok(body) => body,
-            Err(error) => {
-                println!("{}", error);
-                return HttpResponse::InternalServerError().finish();
-            }
-        };
-
-        web::Json(Journal {
-            title: journal_title,
-            body: journal_body
-        }).respond_to(&req).map_into_boxed_body()
+    if trusted_user_uuid != journal_user_uuid {
+        return HttpResponse::Unauthorized().finish();
     }
+
+    let journal_title: String = match journal_entry_row.try_get("title") {
+        Ok(title) => title,
+        Err(error) => {
+            println!("{}", error);
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
+
+    let journal_body: String = match journal_entry_row.try_get("title") {
+        Ok(body) => body,
+        Err(error) => {
+            println!("{}", error);
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
+
+    web::Json(Journal {
+        title: journal_title,
+        body: journal_body
+    }).respond_to(&req).map_into_boxed_body()
 }
