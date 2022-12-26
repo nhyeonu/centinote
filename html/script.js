0c318e0ae8f4907d5a598157c23ecc07d87028e8
nhyeonu <uccccu10@gmail.com>
2022-12-26 11:37:30 +0900

    refactor[WEB]: Move all js scripts into a single file

---
+++ html/script.js
@@ -0,0 +1,46 @@
+function submitForm(form) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(form.getAttribute("method"), form.getAttribute("action"));
+
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    var data = {};
+
+    let inputs = form.getElementsByTagName("input");
+    for (const input of inputs) {
+        data[input.getAttribute("name")] = input.value;
+    }
+
+    let textareas = form.getElementsByTagName("textarea");
+    for (const textarea of textareas) {
+        data[textarea.getAttribute("name")] = textarea.value;
+    }
+    
+    xhr.send(JSON.stringify(data));
+}
+
+function get_cookie_value(target_name) {
+    const cookies = document.cookie.split("; ");
+    const target_cookie = cookies.find((cookie) => cookie.startsWith(target_name + "="));
+    const value = target_cookie?.split("=")[1];
+    return value;
+}
+
+const query_string = window.location.search;
+const url_parameters = new URLSearchParams(query_string);
+
+const user_uuid = get_cookie_value("user_uuid");
+const editor_form = document.getElementById("editor-form");
+
+if(editor_form !== null) {
+    const entry_uuid = url_parameters.get("entry-uuid");
+
+    if(entry_uuid !== null) {
+        editor_form.setAttribute("method", "PUT");
+        editor_form.setAttribute("action", "/api/journals/" + entry_uuid);
+    } else {
+        editor_form.setAttribute("method", "POST");
+        editor_form.setAttribute("action", "/api/users/" + user_uuid + "/journals");
+    }
+}
