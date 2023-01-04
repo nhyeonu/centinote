154e33cbbdc862207e6cc80ed9892f1d31cd4d21
nhyeonu <uccccu10@gmail.com>
2023-01-05 07:47:53 +0900

    feat[WEB]: Implement login error messages triggered by HTTP responses

--- html/login.js
+++ html/login.js
@@ -9,19 +9,29 @@ function setFormWarning(description, focus_element) {
 }
 
 function submitLogin() {
+    const username_input_element = document.getElementById("username");
+    const password_input_element = document.getElementById("password");
+    const submit_input_element = document.getElementById("submit");
+
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "/api/login");
     xhr.setRequestHeader("Accept", "application/json");
     xhr.setRequestHeader("Content-Type", "application/json");
 
     xhr.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status > 99 && this.status < 300) {
-            window.location.href = "/timeline.html";
+        if(xhr.readyState == 4) {
+            if(xhr.status > 99 && xhr.status < 300) {
+                window.location.href = "/timeline.html";
+            } else if(xhr.status == 401) {
+                setFormWarning("Username and/or password is incorrect.", username_input_element);
+            } else {
+                setFormWarning(
+                    "Something has gone wrong! " +
+                    "Please contact the server admin if the problem persists.",
+                    submit_input_element);
+            }
         }
     };
-
-    const username_input_element = document.getElementById("username");
-    const password_input_element = document.getElementById("password");
 
     const data = {};
     data.username = username_input_element.value;
